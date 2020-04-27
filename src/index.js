cc21df6245595083f2be4329cb2038dd0e8ea2e2
Antoine CARON <antoine.caron@bedrockstreaming.com>
2020-04-27 13:22:22 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import { useState, useCallback, useEffect } from "react";
+
+const REDUCE_MOTION_MEDIA = "(prefers-reduced-motion: reduce)";
+
+const mediaQuery = window && window.matchMedia(REDUCE_MOTION_MEDIA);
+
+export const useReduceMotion = () => {
+  const [state, setState] = useState();
+
+  const listener = useCallback(() => {
+    setState(!!mediaQuery.matches);
+  }, []);
+
+  useEffect(() => {
+    setState(!!mediaQuery.matches);
+    mediaQuery.addEventListener("change", listener);
+    return () => {
+      mediaQuery.removeEventListener("change", listener);
+    };
+  }, []);
+
+  return state;
+};
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    libraryTarget: "umd",
+    library: "useReduceMotion",
+    umdNamedDefine: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+    ],
+  },
+  externals: [
+      /react/
+  ]
+};
