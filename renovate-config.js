8b61e5f0dba546f87bbe120852047c1f1701908c
Antoine Caron <antoine.caron@bedrockstreaming.com>
2022-04-12 08:14:19 +0200

    poc: use renovate with github action

---
+++ renovate-config.js
@@ -0,0 +1,26 @@
+module.exports = {
+    dryRun: true,
+    username: 'renovate-release',
+    gitAuthor: 'Renovate Bot <bot@renovateapp.com>',
+    onboarding: false,
+    platform: 'github',
+    includeForks: true,
+    repositories: [
+        'Slashgear/use-reduced-motion',
+    ],
+    packageRules: [
+        {
+            description: 'lockFileMaintenance',
+            matchUpdateTypes: [
+                'pin',
+                'digest',
+                'patch',
+                'minor',
+                'major',
+                'lockFileMaintenance',
+            ],
+            dependencyDashboardApproval: false,
+            stabilityDays: 0,
+        },
+    ],
+};
